0abfac1c449d3e87b410cbecf416669fd4fc22ac
Umar Mataev <u.mataev@pooling.me>
2023-10-31 22:47:12 +0300

    BGZ-1.5.1 фикс когда генерировались дополнительные файлы к scss

--- Config/build/buildPlugins.ts
+++ Config/build/buildPlugins.ts
@@ -11,8 +11,8 @@ export function buildPlugins({paths}: BuildOptions): webpack.WebpackPluginInstan
         }),
         new webpack.ProgressPlugin(),
         new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash.8].css',
-            chunkFilename: 'css/[name].[contenthash.8].css'
+            filename: 'css/[name].[contenthash:8].css',
+            chunkFilename: 'css/[name].[contenthash:8].css'
         })
     ]
 }
