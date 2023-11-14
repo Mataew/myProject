dd0a2c916a36194d8da661fd3eb0c018f41b48b2
Mataev Umar <mister.umars@mail.ru>
2023-11-14 16:18:01 +0300

    BGZ-1.7.0 Организация стилей. Добавление тем.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React, {Suspense} from 'react';
-import Counter from './components/Counter';
+import React, {Suspense, useContext, useState} from 'react';
 import {Routes, Route, Link} from 'react-router-dom';
-import './index.scss';
+import './styles/index.scss';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {Theme, ThemeContext} from "./theme/ThemeContext";
+import {useTheme} from "./theme/useTheme";
+
+
 
 const App = () => {
+
+    const {theme, toggleTheme} = useTheme();
+
     return (
-        <div className="app">
+        <div className={`app ${theme}`}>
+            <button onClick={toggleTheme}>TOggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О нас</Link>
             <Suspense fallback={<div>Loading...</div>}>
