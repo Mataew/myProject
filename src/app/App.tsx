d667739a2c8d75e219541c7fa95e09ff5468bade
Mataev Umar <mister.umars@mail.ru>
2024-01-10 14:14:03 +0300

    BGZ-2.1.0 Настройка AppRouter'a

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,6 +5,7 @@ import { useTheme } from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
 import {AboutPage} from "pages/AboutPage";
 import {MainPage} from "pages/MainPage";
+import {AppRouter} from "app/providers/router";
 
 
 
@@ -17,12 +18,7 @@ const App = () => {
             <button onClick={toggleTheme}>TOggle</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О нас</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage/>}/>
-                    <Route path={'/'} element={<MainPage/>}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     );
 };
