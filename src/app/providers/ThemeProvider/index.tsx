ffb0f9060c981be161923a1ede9eb4048cb245d4
Mataev Umar <mister.umars@mail.ru>
2024-01-11 17:43:55 +0300

    Svg loader. File loader. Button UI kit

--- src/app/providers/ThemeProvider/index.tsx
+++ src/app/providers/ThemeProvider/index.tsx
@@ -1,5 +1,5 @@
 import ThemeProvider from "./ui/ThemeProvider";
 import { useTheme } from "./lib/useTheme";
-import {ThemeContext} from "app/providers/ThemeProvider/lib/ThemeContext";
+import {Theme} from "./lib/ThemeContext";
 
-export { ThemeProvider, useTheme, ThemeContext };
+export { ThemeProvider, useTheme, Theme };
