3b640e56d75c19d012c4bdf2210dcab7978e4e1d
Mataev Umar <mister.umars@mail.ru>
2024-01-11 16:22:24 +0300

    BGZ-2.2.0 Navbar. Шаблоны для разработки. Первый UI Kit компонент

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,9 @@
-import React, {Suspense} from 'react';
-import {Routes, Route, Link} from 'react-router-dom';
+import React from 'react';
 import './styles/index.scss';
 import { useTheme } from "app/providers/ThemeProvider";
 import {classNames} from "shared/lib/classNames/classNames";
-import {AboutPage} from "pages/AboutPage";
-import {MainPage} from "pages/MainPage";
 import {AppRouter} from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 
 
@@ -15,10 +13,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOggle</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О нас</Link>
+            <Navbar />
             <AppRouter/>
+            <button onClick={toggleTheme}>TOggle</button>
         </div>
     );
 };
